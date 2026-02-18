b7c85424cbab4005f88fd81975b314e7045ef9fe
Sanidhya Pathak <pathaksanidhya14@gmail.com>
2026-02-18 21:46:07 +0530

    Add files via upload

--- Find_Missing.cpp
+++ Find_Missing.cpp
@@ -1,9 +1,44 @@
-#include<iostream>
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     int n;
+//     cout<<"Enter Value of n : ";
+//     cin>>n;
+
+//     int arr[n-1];
+//     cout << "Enter " << n-1 << " elements: ";
+
+//     for(int i = 0; i < n-1; i++) {
+//         cin >> arr[i];
+//     }
+
+//     // calculating total sum : 
+
+//     int totalSum=n*(n+1)/2;
+
+//     // calculating arraysum : 
+//     int arrSum=0;
+//     for(int i=0; i<n-1;i++){
+//         arrSum=arrSum+arr[i];
+//     }
+
+//     // Missing Value 
+
+//     int Missing=totalSum-arrSum;
+
+//     cout<<"Missing value is : "<<Missing;
+
+
+//    return 0;
+// }
+
+#include <iostream>
 using namespace std;
-int main(){
+
+int main() {
     int n;
-    cout<<"Enter Value of n : ";
-    cin>>n;
+    cout << "Enter total count including missing: ";
+    cin >> n;
 
     int arr[n-1];
     cout << "Enter " << n-1 << " elements: ";
@@ -12,22 +47,30 @@ int main(){
         cin >> arr[i];
     }
 
-    // calculating total sum : 
+    // Find smallest and largest element
+    int min = arr[0];
+    int max = arr[0];
 
-    int totalSum=n*(n+1)/2;
+    for(int i = 1; i < n-1; i++) {
+        if(arr[i] < min)
+            min = arr[i];
 
-    // calculating arraysum : 
-    int arrSum=0;
-    for(int i=0; i<n-1;i++){
-        arrSum=arrSum+arr[i];
+        if(arr[i] > max)
+            max = arr[i];
     }
 
-    // Missing Value 
+    // Expected sum using arithmetic progression formula
+    int expectedSum = (min + max) * n / 2;
 
-    int Missing=totalSum-arrSum;
+    // Actual array sum
+    int arrSum = 0;
+    for(int i = 0; i < n-1; i++) {
+        arrSum += arr[i];
+    }
 
-    cout<<"Missing value is : "<<Missing;
+    int missing = expectedSum - arrSum;
 
+    cout << "Missing number is: " << missing;
 
-   return 0;
+    return 0;
 }
